9c02550278779d6093091a88c0591d4722c731c5
TSMARAMWIDZE <>
2026-01-16 12:33:37 +0000

    Boolean Operators

---
+++ conditional_statements/boolean_operators/main.py
@@ -0,0 +1,10 @@
+# define a boolean variable, assign a value and compare it using the greater than or equal to (>=) operator 
+
+total_cost = 25.00
+discount_threshhold = 20.00
+discountEligible = total_cost >= discount_threshhold
+
+print("Is the purchase eligible for a discount?", discountEligible)
+
+
+
