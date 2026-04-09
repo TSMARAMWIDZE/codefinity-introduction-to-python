7245b2e4bc2412a5d3f010116daf2ba699a9a47c
TSMARAMWIDZE <>
2026-04-09 21:16:44 +0000

    Challenge: Inventory Sorting Capstone

--- variables_and_types/challenge_inventory_sorting_capstone/main.py
+++ variables_and_types/challenge_inventory_sorting_capstone/main.py
@@ -1,23 +1,18 @@
 # Lists of items and categories for slicing
 items = "Bubblegum, Chocolate, Pasta"
 categories = "Candy Aisle, Pasta Aisle"
+# Slicing items string to extract
+candy1 = items[0:9] #'Bubblegum'
+candy2 = items[10:20] #'Chocolate'
+dry_goods = items[21:] #'Pasta'
+# Slicing categories string to extract
+category1 = categories[0:11] # 'Candy Aisle'
+category2 = categories[12:24] # 'Pasta Aisle'
+# Creating price variables
+bubblegum_price = "$1.50"
+chocolate_price = "$2.00"
+pasta_price = "$5.40"
 
-# Slice the `items` string into individual items
-candy1 = items[:9]        # Bubblegum
-candy2 = items[11:20]     # Chocolate
-dry_goods = items[22:]     # Pasta
-
-# Slice the `categories` string into individual categories
-category1 = categories[:11]   # Candy Aisle
-category2 = categories[13:]   # Pasta Aisle
-
-# Replace the blank placeholders (`___`) with the names of the variables 
-# to store the prices of each item
-bubblegum_price = "$1.50"  # Bubblegum costs 1.50 dollars
-chocolate_price = "$2.00"  # Chocolate costs 2.00 dollars
-pasta_price = "$5.40"      # Pasta costs 5.40 dollars
-
-# Create print statements that combine the items, their prices, and categories 
-print("We have " + candy1 + " for " + bubblegum_price + " in the " + category1)
-print("We have " + candy2 + " for " + chocolate_price + " in the " + category1)
-print("We have " + dry_goods + " for " + pasta_price + " in the " + category2)
+print("We have " + candy1 + " " +"for"+ " " + bubblegum_price + " in the " + category1)
+print("We have" + candy2 + " " +"for"+ " " + chocolate_price + " in the " + category1)
+print(F"We have{dry_goods} for {pasta_price} in the{category2}")
